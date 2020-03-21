4db1692d4cd9b0f892d8c85b4c852e143112cd02
Koti Rama Murthy Davuluri <kotisofficial@gmail.com>
2020-03-20 20:04:20 -0600

    fixing ports

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-var app = express()
+var app = express();/*
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))*/
 const https = require("https");
 const url = "https://corona.lmao.ninja/all";
 
-app.listen(port);
-
-console.log('todo list RESTful API server started on: ' + port);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+console.log('todo list RESTful API server started on: ' + PORT);
 
 app.get("/url", (req, res, next) => {
 	https.get(url, rs => {
