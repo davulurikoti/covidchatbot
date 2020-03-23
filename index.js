925ee4edd03cde2f6c9ab46e3b3f8e061d0b0e76
Koti Rama Murthy Davuluri <kotisofficial@gmail.com>
2020-03-22 22:28:59 -0600

    my countries

--- index.js
+++ index.js
@@ -44,23 +44,6 @@ app.get("/url", (req, res, next) => {
 });
 });
 
-function getCity(num){
-	var id = num.split("+")[1];
-	var options = { method: 'GET',
-  	url: 'https://covid-8840.restdb.io/rest/members?q={"number":"'+id+'"}',
-  	headers: 
-   	{ 'cache-control': 'no-cache',
-    	 'x-apikey': '5febb36d47b1aa8004dc42c3fd6f20d0c5b5d' } };
-
-	request(options, function (error, response, body) {
-  	if (error) throw new Error(error);
-  	if(JSON.parse(body).length == 0){
-  		return null;
-  	}
-  	return JSON.parse(body).city;
-	});
-}
-
 app.use(session({secret: 'anything-you-want-but-keep-secret'}));
 
 app.post('/sms', (req, res) => {
@@ -103,6 +86,35 @@ app.post('/sms', (req, res) => {
  	}
  	else if(req.body.Body == 2){
  		//check the country exists or not
+ 		req.session.current = 1;
+ 		var id = req.body.From.split("+")[1];
+		var options = { method: 'GET',
+  		url: 'https://covid-8840.restdb.io/rest/members?q={"number":"'+id+'"}',
+  		headers: 
+   			{ 'cache-control': 'no-cache',
+    	 	'x-apikey': '5febb36d47b1aa8004dc42c3fd6f20d0c5b5d' } };
+
+		request(options, function (error, response, body) {
+  			if (error) throw new Error(error);
+
+  			axios.get('https://corona.lmao.ninja/countries/'+JSON.parse(body).country)
+  			.then(response => {
+    
+    		message = response.data.country+'\n -------------------\n Cases:'+response.data.cases+'\n Today cases:'+response.data.todayCases+'\n Deaths:'+response.data.deaths+'\n Today deaths:'+response.data.todayDeaths+'\n Recovered: '+response.data.recovered+'\n Active:'+response.data.active+'\n Critical:'+response.data.critical+'\n Cases per million:'+response.data.casesPerOneMillion;
+    		twiml.message(message);
+
+  			res.writeHead(200, {'Content-Type': 'text/xml'});
+ 			res.end(twiml.toString());
+  		})
+  		.catch(error => {
+   		 console.log(error);
+  		});
+  	
+	});
+		twiml.message("You haven't set your country. Please enter your country name here");
+
+  			res.writeHead(200, {'Content-Type': 'text/xml'});
+ 			res.end(twiml.toString());
  		/*var city = getCity(req.body.FROM);
  		if(null != null){
  			axios.get('https://corona.lmao.ninja/countries/'+city)
@@ -131,11 +143,11 @@ app.post('/sms', (req, res) => {
   			res.writeHead(200, {'Content-Type': 'text/xml'});
  			res.end(twiml.toString());
  		}*/
- 		req.session.current = 1;
+ 		/*req.session.current = 1;
  		twiml.message("This feature is under development. Coming soon...\n Select from main menu again. \n"+mainMenu);
 
   		res.writeHead(200, {'Content-Type': 'text/xml'});
- 		res.end(twiml.toString());
+ 		res.end(twiml.toString());*/
  	}
  	else if(req.body.Body == 3){
  		req.session.current = 10;
